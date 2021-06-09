0f47e8251b10dd2ff999c78592a5254736b3e96d
Ken McHugh <kmchugh@icatalyst.com>
2021-06-09 11:12:20 +0800

    Fix for footer in safari

--- layouts/components/FooterLayouts/Footer.js
+++ layouts/components/FooterLayouts/Footer.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import {AppBar, Toolbar,Typography} from '@material-ui/core';
 import config from 'app/settings/config';
-
 import clsx from 'clsx';
 import {makeStyles} from '@material-ui/styles';
 
-const useStyles = makeStyles(() => ({
-  root  : {
-    // TODO: Fix, this is currently required for safari
-    flexBasis : '32px!important'
-  }
-}));
+const useStyles = makeStyles((theme) => {
+  return {
+    root : {
+      // TODO: Find out why safari needs this to show the footer
+      flexBasis: `${theme.spacing(4)}px!important`,
+      flexShrink: 0,
+      flex: 1,
+      flexGrow: 0,
+      position: 'relative',
+      zIndex: 10
+    },
+  };
+});
 
 
 function Footer()
 {
-  const {footer} = config.text;
   const classes = useStyles();
+  const {footer} = config.text;
 
   return (
-    <AppBar id="app-footer" className={clsx(classes.root, 'relative z-10 flex-1 flex-grow-0')} color="default">
+    <AppBar id="app-footer" className={clsx(classes.root)} color="default">
       <Toolbar className="px-16 py-8 md:py-0 flex items-center min-h-32 text-10 md:text-12">
         <Typography variant="caption">
           {footer}
