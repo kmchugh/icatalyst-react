63be1751334d06cd5215fdea237c4093ac47d5ed
Ken McHugh <kmchugh@icatalyst.com>
2021-06-16 11:06:17 +0800

    Dialog Actions tests

---
+++ store/actions/dialog.actions.test.js
@@ -0,0 +1,29 @@
+import * as actions from './dialog.actions';
+import {OPEN_DIALOG, CLOSE_DIALOG} from './dialog.actions';
+
+
+describe('actions', () => {
+
+  it('opens dialog', () => {
+
+    const options = 'open';
+    const expectedAction = {
+      type:OPEN_DIALOG,
+      payload : {
+        options : 'open'
+      }
+    };
+
+    expect(actions.openDialog(options)).toEqual(expectedAction);
+  });
+
+  it('closes dialog', () => {
+
+    const expectedAction = {
+      type:CLOSE_DIALOG,
+    };
+
+    expect(actions.closeDialog()).toEqual(expectedAction);
+  });
+
+});
