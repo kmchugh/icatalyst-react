ff1dbdef2378eae24ed64a48ccbc94a736c44782
Ken McHugh <kmchugh@icatalyst.com>
2021-06-24 13:21:08 +0800

    Removal of conflicting index.js

--- store/actions/index.js
+++ store/actions/index.js
@@ -3,5 +3,3 @@ export * from './settings.actions';
 export * from './message.actions';
 export * from './navbar.actions';
 export * from './navigation.actions';
-
-export * from '../../components/Singularity/store/actions';
