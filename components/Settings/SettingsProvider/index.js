2da859d6ee4225a2315a367a585a3b8d0c033a56
manamiCE <manami.majumdar@cognitive-edge.com>
2022-02-23 12:07:19 +0530

    Check for settings.instanceValues

--- components/Settings/SettingsProvider/index.js
+++ components/Settings/SettingsProvider/index.js
@@ -54,7 +54,7 @@ export function useSettingsContext(id, instanceProps) {
       // override with the user set defaults
       settings.defaultValues,
       // override with the specified instance values
-      instanceID ?
+      instanceID && settings.instanceValues ?
         settings.instanceValues[instanceID] : {}
     );
 
