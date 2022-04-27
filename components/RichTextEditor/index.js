d55f7803014ea185e3d783b579484197bcb62ca4
manamiCE <manami.majumdar@cognitive-edge.com>
2022-04-27 15:26:20 +0800

    testing with custom editor

--- components/RichTextEditor/index.js
+++ components/RichTextEditor/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {makeStyles} from '@material-ui/styles';
 import clsx from 'clsx';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import Editor from '@ckeditor/ckeditor5-build-balloon';
+import Editor from 'ckeditor5-custom-build';
 import _ from '@icatalyst/@lodash';
 
 // Uncomment this to quickly see a list of plugins
