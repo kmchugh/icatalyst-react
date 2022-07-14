e9a1b4b356fcf966b746f5a54f786272d26154ad
Ken McHugh <kmchugh@icatalyst.com>
2022-07-14 08:53:10 +0800

    Adding jest testing

---
+++ packages/components/src/static-init.spec.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+const init = require('./static-init');
+
+describe('static-init', () => {
+    it('should be defined', () => {
+        expect(init).toBeDefined();
+    });
+});
---
+++ packages/core/src/libs/@history/@history.spec.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+const _history = require('.');
+
+describe('history', () => {
+    it('should be defined', () => {
+        expect(_history).toBeDefined();
+    });
+});
