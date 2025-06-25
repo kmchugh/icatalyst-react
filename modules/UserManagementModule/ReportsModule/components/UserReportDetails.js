f964cd068c34de1c8007818a317240393481c425
Kiran Dudhat <kiran.dudhat@icatalyst.com>
2025-06-25 15:29:09 +0530

    change makestyles for createMuiStyles and clsx to cxMui

--- modules/UserManagementModule/ReportsModule/components/UserReportDetails.js
+++ modules/UserManagementModule/ReportsModule/components/UserReportDetails.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {makeStyles} from '@mui/styles';
-import clsx from 'clsx';
 import Typography from '@mui/material/Typography';
 import moment from '../../../../@moment';
 import _ from '../../../../@lodash';
 import pluralize from 'pluralize';
 import ListItemText from '@mui/material/ListItemText';
 import {DialogContent} from '../../../../components/Dialogs';
+import { createMuiStyles, cxMui } from '../../../../utilities';
 
-const useStyles = makeStyles((theme)=>{
+const useStyles = createMuiStyles((theme)=>{
   return {
     root : {
     },
@@ -63,32 +62,32 @@ const UserResourceSection = ({
 
   return (
     <div
-      className={clsx(styles.resourceSection, className)}
+      className={cxMui(styles.resourceSection, className)}
       style={{...style}}
     >
 
       <Typography
         variant="h5"
         color="primary"
-        className={clsx(styles.sectionTitle)}
+        className={cxMui(styles.sectionTitle)}
       >
         {_.startCase(pluralize(title))} ({data.length})
       </Typography>
 
-      <div className={clsx(styles.resourceWrapper)}>
+      <div className={cxMui(styles.resourceWrapper)}>
 
-        <div className={clsx(styles.ownerSection)}>
+        <div className={cxMui(styles.ownerSection)}>
           <Typography
             variant="h6"
           >
             Owns ({ownerData.length})
           </Typography>
 
-          <div className={clsx(styles.resourceList)}>
+          <div className={cxMui(styles.resourceList)}>
             {
               ownerData.length > 0 ? ownerData.map((item)=>(
                 <ListItemText
-                  className={clsx(styles.listItem)}
+                  className={cxMui(styles.listItem)}
                   key={item.resourceID}
                   primary={item.name}
                   secondary={item.resourceID}
@@ -98,18 +97,18 @@ const UserResourceSection = ({
           </div>
         </div>
 
-        <div className={clsx(styles.memberSection)}>
+        <div className={cxMui(styles.memberSection)}>
           <Typography
             variant="h6"
             color="primary"
           >
             Member Of ({memberData.length})
           </Typography>
-          <div className={clsx(styles.resourceList, className)}>
+          <div className={cxMui(styles.resourceList, className)}>
             {
               memberData.map((item)=>(
                 <ListItemText
-                  className={clsx(styles.listItem)}
+                  className={cxMui(styles.listItem)}
                   key={item.resourceID}
                   primary={item.name}
                   secondary={item.resourceID}
@@ -150,7 +149,7 @@ const UserReportDetails = ({
 
   return (
     <DialogContent
-      className={clsx(styles.root, className)}
+      className={cxMui(styles.root, className)}
       style={{...style}}
     >
       <Typography
@@ -172,7 +171,7 @@ const UserReportDetails = ({
         User Since: {moment(user.created).format('LL')}
       </Typography>
 
-      <div className={clsx(styles.resourceContent)}>
+      <div className={cxMui(styles.resourceContent)}>
         {
           sectionKeys.map((key)=>{
             if (!user[key]) {
