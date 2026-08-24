9b0c35c5ae7b0acf4abbfe70b70eb5d84ea89fa5
jaina95 <jainatech25@gmail.com>
2026-08-24 12:28:37 +0530

    Implemented the invite endpoint in the Collaborator

--- components/Singularity/store/actions/resourceInvite.actions.js
+++ components/Singularity/store/actions/resourceInvite.actions.js
@@ -1,5 +1,11 @@
 import {generateActions} from '@icatalyst/utilities/generateActions';
-import {generateOperations} from '@icatalyst/utilities/generateOperations';
+import {
+  createURI,
+  generateOperations,
+  makeReducerRequest,
+  parseToken
+} from '@icatalyst/utilities/generateOperations';
+import {buildResourceInvitePayload} from './resourceInvitePayload';
 
 import URIService from '@icatalyst/services/URIService';
 
@@ -8,5 +14,35 @@ export const actions = generateActions('resourceInvite');
 export const operations = generateOperations({
   uri: ()=>{
     return URIService.getURI('singularity', 'resource_membership');
+  },
+  ADD_ENTITY : (entity, callback, requestConfig = {})=>{
+    const {params = {}} = requestConfig;
+    const queryParams = {...params};
+    delete queryParams.type;
+    delete queryParams.resourceType;
+    delete queryParams.resourceid;
+    delete queryParams.resourceID;
+    delete queryParams.description;
+    delete queryParams.resourceDescription;
+
+    const url = createURI(
+      `${URIService.getURI('singularity', 'invites')}/bulk`,
+      queryParams
+    );
+
+    return makeReducerRequest({
+      method : 'post',
+      url,
+      headers : {
+        Authorization : parseToken(requestConfig),
+        'Content-Type': 'application/json',
+      },
+      data : buildResourceInvitePayload(entity, params),
+      transform : requestConfig.transform,
+    },
+    actions['ENTITY_ADDED'],
+    actions['ENTITY_ADDED_ERROR'],
+    callback
+    );
   }
 }, actions);
